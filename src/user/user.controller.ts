8fff2832ea087c2d3f74d724740ab51383ff3883
Lucky Prabath <lucky.prabath94@gmail.com>
2023-12-19 20:22:48 +0530

    add user guards

---
+++ src/user/user.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard/jwt.guard';
+
+@Controller('users')
+export class UserController {
+
+    
+    @UseGuards(JwtGuard)
+    @Get('me')
+    getMe(@Req() req: Request) {
+        console.log({
+            user: req.user,
+        })
+        return 'user info';
+    }
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { PassportModule } from '@nestjs/passport';
 
-@Module({})
+
+@Module({
+  imports: [PassportModule,],
+  controllers: [UserController]
+})
 export class UserModule {}
